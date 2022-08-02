1c1c2b78e06f3bf3afa7bbaea331a8f4d359593f
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-02 15:04:27 +1000

    Project editing

--- src/Level1.java
+++ src/Level1.java
@@ -1,97 +1,44 @@
 public class Level1 {
-    public static String[] testFunc(String str, int len) {
-        double strLen = str.length();
-        int size = (int)(strLen / len + 1);
-        if (strLen / len != (int)(strLen / len)) {
-            size++;
-        }
-
-        String[] tst = new String[size];
-        for (int i = 0; i < tst.length; i++) {
-            tst[i] = "";
-        }
-
-        char[] ca = str.toCharArray();
-        int t = 1;
-        int u = 0;
-
-        for (int i = 0; i < strLen; i++) {
-            if (u < len * t) {
-                tst[t - 1] += ca[i];
-                u++;
-            }
-            if (u == len * t) {
-                t++;
-            }
-        }
-
-        return tst;
-
-    }
     public static int[] WordSearch(int len, String s, String subs) {
-        String[] str = s.split(" ");
-        String[] strTemp = new String[str.length];
-        String[] strFin = new String[str.length];
-        int t = 1;
+        String[] mas = new String[s.length() / len + 1];
+        String str = s;
 
-        for (int i = 0; i < str.length; i++) {
-            if (str[i].length() > len * t) {
-                String[] mas = testFunc(str[i], len);
-                strTemp[i + t - 1] = mas[t - 1];
-                t++;
+        int t = 0;
+        while (str.length() != 0) {
+            int ind = str.lastIndexOf(' ', len - 1);
+            int sLen = str.length();
+
+            if (ind != -1) {
+                mas[t] = str.substring(0, ind);
+                str = str.substring(ind + 1);
+
+            } else if (sLen >= len) {
+                mas[t] = str.substring(0, len);
+                str = str.substring(len + 1);
 
             } else {
-                strTemp[i] = str[i];
+                mas[t] = str.substring(0, sLen);
+                str = str.substring(sLen);
             }
 
+            t++;
         }
 
-        for (int i = 0; i < str.length; i++) {
-            strFin[i] = "";
-        }
-
-        int tmpCount = 0;
-        String c;
-
-        for (int i = 0; i < str.length; i++) {
-            if (strFin[tmpCount].equals("")) c = "";
-            else c = " ";
-
-            if (strFin[tmpCount].length() + str[i].length() < len) {
-                strFin[tmpCount] += c + str[i];
-            } else {
-                tmpCount++;
-                strFin[tmpCount] += str[i];
-            }
-
-        }
-        int[] ma = new int[tmpCount + 1];
-
+        int[] ma = new int[mas.length];
         for (int i = 0; i < ma.length; i++) {
-            if (strFin[i].matches("(?s).*\\b" + subs + "\\b.*")) {
+            if (mas[i].matches("(?s).*\\b" + subs + "\\b.*")) {
                 ma[i] = 1;
             } else
                 ma[i] = 0;
         }
-
 
         return ma;
     }
 
     public static void main(String[] args) {
         int len = 12;
-        String s = "1) разбивается на набор строк через выравнивание по заданной ширине. строк строк пристрочной 123456789112";
+        String s = "1) строка разбивается на набор строк через выравнивание по заданной ширине.";
         String subs = "строк";
-        testFunc("123456789012 123456789012 12345678910", len);
         WordSearch(len, s, subs);
-
-        String[] oo = new String[100];
-
-        for (int i = 0; i < 100; i++) {
-            if (s.indexOf(" ") <= len) {
-                oo[i] = s.substring(0, s.indexOf(" "));
-                s = s.substring(s.indexOf(" ") + 1);
-            }
-        }
     }
 }
