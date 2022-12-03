b4d9aeaa4b70b7cb4598b5a82a25ab00d39975a5
Andrey Otryaskin <kinlydog@gmail.com>
2022-12-04 01:16:29 +1000

    Update project

--- src/Level1.java
+++ src/Level1.java
@@ -1,28 +1,29 @@
 public class Level1 {
     public static int[] WordSearch(int len, String s, String subs) {
-        String[] strSplit = s.split(" ");
+        String[] wordsFromString = s.split(" ");
         StringBuilder bld = new StringBuilder();
 
-        for (int i = 0; i < strSplit.length; i++) {
-            while (strSplit[i].equals("")) {
+        for (int i = 0; i < wordsFromString.length; i++) {
+            while (wordsFromString[i].equals("")) {
                 i++;
             }
 
-            while (strSplit[i].length() > len) {
-                bld.append(strSplit[i].substring(0, len) + " ");
-                strSplit[i] = strSplit[i].substring(len);
+            while (wordsFromString[i].length() > len) {
+                bld.append(wordsFromString[i].substring(0, len) + " ");
+                wordsFromString[i] = wordsFromString[i].substring(len);
             }
 
-            bld.append(strSplit[i]);
+            bld.append(wordsFromString[i]);
 
-            if (i < strSplit.length - 1) {
+            if (i < wordsFromString.length - 1) {
                 bld.append(" ");
             }
         }
+
         String str = bld.toString();
 
         int masLen = str.length() / (len / 2) + 1;
-        String[] mas = new String[masLen]; // ?
+        String[] limitedRows = new String[masLen]; // ?
 
         int stringNumber = 0;
         while (str.length() != 0) {
@@ -30,33 +31,33 @@ public class Level1 {
             int sLen = str.length();
 
             if (sLen <= len) {
-                mas[stringNumber] = str.substring(0, sLen);
+                limitedRows[stringNumber] = str.substring(0, sLen);
                 str = str.substring(sLen);
             }
 
             else if (str.charAt(len) == ' ') {
-                mas[stringNumber] = str.substring(0, len);
+                limitedRows[stringNumber] = str.substring(0, len);
                 str = str.substring(len + 1);
             }
 
             else if (ind != -1) {
-                mas[stringNumber] = str.substring(0, ind);
+                limitedRows[stringNumber] = str.substring(0, ind);
                 str = str.substring(ind + 1);
             }
 
             stringNumber++;
         }
 
-        int[] ma = new int[stringNumber];
-        for (int i = 0; i < ma.length; i++) {
-            if (mas[i].matches("(?s).*\\b" + subs + "\\b.*")) {
-                ma[i] = 1;
+        int[] checkInKey = new int[stringNumber];
+        for (int i = 0; i < checkInKey.length; i++) {
+            if (limitedRows[i].matches("(?s).*\\b" + subs + "\\b.*")) {
+                checkInKey[i] = 1;
             } else {
-                ma[i] = 0;
+                checkInKey[i] = 0;
             }
         }
 
-        return ma;
+        return checkInKey;
     }
 
 }
